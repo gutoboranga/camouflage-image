f0b8ed4d16fc3adfa5018a442faa2f116e7d9737
Augusto Boranga <gutoboranga@gmail.com>
2019-03-16 18:15:29 -0300

    change server

--- src/server/main.py
+++ src/server/main.py
@@ -1,11 +1,13 @@
+import os
 import tornado.ioloop
 import tornado.web
 
 from server import create_app
 
 app = create_app()
+port = int(os.environ.get("PORT", 5000))
 app.listen(5000)
 
-print('> Running on port 5000')
+# print('> Running on port 5000')
 
 tornado.ioloop.IOLoop.current().start()
