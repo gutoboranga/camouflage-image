6dd79401a5a165f700e82013ddc929a79317e865
Augusto Boranga <gutoboranga@gmail.com>
2019-03-17 22:09:47 -0300

    remove futures from requirements.txt

--- src/server/main.py
+++ src/server/main.py
@@ -8,6 +8,6 @@ app = create_app()
 port = int(os.environ.get("PORT", 5000))
 app.listen(5000)
 
-# print('> Running on port 5000')
+print('> Running on port 5000')
 
 tornado.ioloop.IOLoop.current().start()
