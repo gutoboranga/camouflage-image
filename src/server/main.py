1b1a5b6ea2f90d9f3a2ee7c2abe8ea0661df78f4
Augusto Boranga <gutoboranga@gmail.com>
2019-03-24 01:36:11 -0300

    change port to listen

--- src/server/main.py
+++ src/server/main.py
@@ -5,9 +5,10 @@ import tornado.web
 from server import create_app
 
 app = create_app()
-port = int(os.environ.get("PORT", 5000))
-app.listen(5000)
 
-print('> Running on port 5000')
+port = int(os.environ.get('PORT', 4000))
+app.listen(port)
+
+print('> Running on port {}'.format(port))
 
 tornado.ioloop.IOLoop.current().start()
